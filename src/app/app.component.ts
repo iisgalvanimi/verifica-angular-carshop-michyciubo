4a602c533df94020cda3df9a8980a8f35444191c
michyciubo <ciubotaru.mihail@iisgalvanimi.edu.it>
2022-03-10 09:09:09 +0000

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Angular-Empty-Project';
+
+  testo_salvato:string="(ancora nulla)"
+
+  constructor() {}
+
+  onClick(htmlInput1:HTMLInputElement,htmlInput2:HTMLInputElement,htmlInput3:HTMLInputElement) {
+    console.log(htmlInput1)
+    this.testo_salvato=htmlInput1.value
+  }
+
 }
